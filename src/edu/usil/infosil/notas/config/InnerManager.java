7886cb73a60401751a682c28dc89d10e7b23299f
GeinerGV <geiner120300@gmail.com>
2018-11-28 00:59:46 -0500

    administración interna

--- src/edu/usil/infosil/notas/config/InnerManager.java
+++ src/edu/usil/infosil/notas/config/InnerManager.java
@@ -2,7 +2,15 @@ package edu.usil.infosil.notas.config;
 
 public class InnerManager {
 
+	private static String directorio_base = "";
+
+	private static final String[] DIRECTORIOS = {"src/test/recursos/xml/Cursos.xml"};
+
 	public static String getDownloadCursosPath() {
-		return "src/test/recursos/xml/Cursos.xml";
+		return DIRECTORIOS[0];
 	}
+
+	// TODO: Ejemplos de como configurar el diseño de la ventana
+	private static final int DEFAULT_INIT_TAB = 1;
+	
 }
